2b0968992b0a3ee0cc559ddbc6b4738edf0f00b2
Tekko2000 <ot1977@gmx.net>
2021-02-12 13:23:22 +0100

    korrektur

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,19 @@
-var txt = document.getElementById("txt");
-var warning = document.getElementById("warning");
-var result = document.getElementById("result");
+// var txt = document.getElementById("txt");
+// var warning = document.getElementById("warning");
+// var result = document.getElementById("result");
 
 function transfer() {
+    var txt = document.getElementById("txt")
+    var warning = document.getElementById("warning")
+    var result = document.getElementById("result")
     if (txt.value == "") {
         warning.innerHTML = "Bitte gib Deine Nachricht ein!"
         warning.style.color = "red"
+        result.style.display = "none"
     } else {
-        result.innerHTML = txt.value;
-        result.style.color = "blue";
+        result.innerHTML = txt.value
+        result.style.color = "blue"
+        result.style.display = "block"
+        warning.innerHTML = ""
     }
 }
